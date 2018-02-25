568f2ca52b106605be48aae586ff2d81744196c5
Cyril Tsiboulski <ct@cloudred.com>
2018-02-25 15:12:12 -0500

    Added title PNG to opening credits

---
+++ Assets/__Scripts/SpriteFader.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpriteFader : MonoBehaviour
+{
+    public float fadeSpeed = 2.0f;
+    public float fadeDelay = 0;
+    public bool fadeOnLoad = false;
+    public bool fadeOutLoad = false;
+
+    IEnumerator FadeInCoroutine;
+    IEnumerator FadeOutCoroutine;
+
+    private void Start()
+    {
+        if (fadeOnLoad)
+        {
+            FadeInSprite();
+        }
+        if (fadeOutLoad)
+        {
+            FadeOutSprite();
+        }
+    }
+
+    public void FadeInSprite()
+    {
+        FadeInCoroutine = FadeIn(GetComponent<SpriteRenderer>()); // create an IEnumerator object
+        StartCoroutine(FadeInCoroutine);
+    }
+    public void FadeOutSprite()
+    {
+        FadeOutCoroutine = FadeOut(GetComponent<SpriteRenderer>()); // create an IEnumerator object
+        StartCoroutine(FadeOutCoroutine);
+    }
+
+    IEnumerator FadeOut( SpriteRenderer _sprite)
+    {
+       yield return new WaitForSeconds(fadeDelay);
+       Color tmpColor = _sprite.color;
+       while (tmpColor.a >0)
+        {
+            tmpColor.a -= Time.deltaTime / fadeSpeed;
+            _sprite.color = tmpColor;
+            if (tmpColor.a <=0)
+            {
+                tmpColor.a = 0f;
+                StopCoroutine(FadeOutCoroutine);
+            }
+            yield return null;
+        }
+        _sprite.color = tmpColor;
+    }
+    IEnumerator FadeIn(SpriteRenderer _sprite)
+    {
+        yield return new WaitForSeconds(fadeDelay);
+        Color tmpColor = _sprite.color;
+        while (tmpColor.a < 1.0f)
+        {
+            tmpColor.a += Time.deltaTime / fadeSpeed;
+            _sprite.color = tmpColor;
+            if (tmpColor.a >= 1.0f)
+            {
+                tmpColor.a = 1.0f;
+                StopCoroutine(FadeInCoroutine);
+            }
+            yield return null;
+        }
+        _sprite.color = tmpColor;
+    }
+}
--- Assets/__Scripts/TitlesController.cs
+++ Assets/__Scripts/TitlesController.cs
@@ -15,6 +15,7 @@ public class TitlesController : MonoBehaviour {
     private float VanityCardDelay;
     private GameObject SceneManager;
     private bool SceneFinished = false;
+    private GameObject QSVR_Title;
 
     IEnumerator RollOpeningCreditsCoroutine;
     IEnumerator FadeInCoroutine;
@@ -27,20 +28,22 @@ public class TitlesController : MonoBehaviour {
             {"", "Hadley Boyd",""},
             {"", "Drew Moore",""},
             {"and", "Michael DeBartolo","as Sebastian"},
-            {"sound mixer", "Laura Laidlaw",""},
-            {"360 video by", "Richard Hammer",""},
+            {"sound mixer", "Laura Cunningham",""},
+            {"360 video production & editing by", "Richard Hammer",""},
             {"depthkit operator", "Supreet Mahanti",""},
             {"director of photography", "Cory Allen",""},
             {"producer", "Kathleen Fox",""},
-            {"creative & technical direction by", "Cyril Tsiboulski",""},
+            {"creative direction & unity development by", "Cyril Tsiboulski",""},
             {"written & directed by", "Illya Szilak",""},
-            {"", "Queerskins:\nA Love Story",""},
+            {"", "",""}, //Queerskins:\nA Love Story
         };
 
     string[,] closingCredits = new string[,]
         {
             
             {"", "The End",""},
+            {"TO DO:", "Closing Credits",""},
+            {"TO DO:", "Closing Credits",""},
             {"TO DO:", "Closing Credits",""},
         };
 
@@ -52,6 +55,12 @@ public class TitlesController : MonoBehaviour {
         NameText.GetComponent<Text>().text = "";
         SubtitleText.GetComponent<Text>().text = "";
         titles = openingTitles;
+
+        //find the title png
+        QSVR_Title = GameObject.Find("QSVR_Title");
+        Color tmpColor = QSVR_Title.GetComponent<SpriteRenderer>().color;
+        tmpColor.a = 0f;
+        QSVR_Title.GetComponent<SpriteRenderer>().color = tmpColor;
     }
     void Start () {
         //Get the Fade Delay value from the Fade Object in/out script attached to teh Fancy game object
@@ -73,21 +82,26 @@ public class TitlesController : MonoBehaviour {
         {
             //for (int i = 0; i <= titles.GetUpperBound(0); i++)
             //{
-            string Title = titles[CurrentCrewArrayPos, 0];
-            string Name = titles[CurrentCrewArrayPos, 1];
-            string Subtitle = titles[CurrentCrewArrayPos, 2];
-            //incremenent titles position in array
-            CurrentCrewArrayPos++;
-
-            //Fade in next crew name
-            FadeInCoroutine = FadeIn(Title, Name, Subtitle); // Create the FadeIn IEnumerator object
-            StartCoroutine(FadeInCoroutine);
-
+            //if this is the last array item and we're not rollwing closing credits show our title PNG
+            if (!SceneFinished && CurrentCrewArrayPos == titles.GetUpperBound(0))
+            {
+                QSVR_Title.GetComponent<SpriteFader>().FadeInSprite();
+            }
+            else
+            {
+                string Title = titles[CurrentCrewArrayPos, 0];
+                string Name = titles[CurrentCrewArrayPos, 1];
+                string Subtitle = titles[CurrentCrewArrayPos, 2];
+                //Fade in next crew name
+                FadeInCoroutine = FadeIn(Title, Name, Subtitle); // Create the FadeIn IEnumerator object
+                StartCoroutine(FadeInCoroutine);
+            }
             //also init the fadeout sequence
             FadeOutCoroutine = FadeOut(WaitTime + delay); // Create the FadeIn IEnumerator object
             StartCoroutine(FadeOutCoroutine);
+            //incremenent titles position in array
+            CurrentCrewArrayPos++;
 
-            //Debug.Log(titles.GetUpperBound(0));
         }
         else
         {
@@ -96,6 +110,7 @@ public class TitlesController : MonoBehaviour {
                 //Tell the scene manager, we're done with titles, only if this was
                 //the openning credits roll
                 EventManager.TriggerEvent("TitlesAreDone");
+                QSVR_Title.GetComponent<SpriteFader>().FadeOutSprite();
             } else
             {
                 //otherwise we are done done!
@@ -104,7 +119,7 @@ public class TitlesController : MonoBehaviour {
             }
            
 
-            //Stop Coroutines and Destroy the Titles Gameobject
+            //Stop Coroutines
             StopCoroutine(FadeInCoroutine);
             StopCoroutine(FadeOutCoroutine);
             //Reset CurrentCrewArrayPos
@@ -117,8 +132,8 @@ public class TitlesController : MonoBehaviour {
     {
         yield return new WaitForSeconds(delay);
         TitleText.GetComponent<Text>().text = CrewTitle;
-        NameText.GetComponent<Text>().text = CrewName;
-        SubtitleText.GetComponent<Text>().text = Subtitle;
+        NameText.GetComponent<Text>().text = CrewName;     
+        SubtitleText.GetComponent<Text>().text = Subtitle;  
         for (float f = 0f; f <= 1; f += 0.01f)
         {
             GetComponent<CanvasGroup>().alpha = f;
@@ -129,7 +144,6 @@ public class TitlesController : MonoBehaviour {
     IEnumerator FadeOut(float delay)
     {
         yield return new WaitForSeconds(delay);
-
         for (float f = 1f; f >= 0; f -= 0.01f)
         {
             GetComponent<CanvasGroup>().alpha = f;
